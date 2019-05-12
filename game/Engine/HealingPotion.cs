1287bfc6e075c915e7baf7440920d0f6bc43c9ea
zzk101999 <50468209+zzk101999@users.noreply.github.com>
2019-05-12 04:30:19 +0200

    Add files via upload

---
+++ game/Engine/HealingPotion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace Engine
+{
+    public class HealingPotion : Item
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string NamePlural { get; set; }
+        public int AmountToHeal { get; set; }
+
+        public HealingPotion(int id, string name, string namePlural, int amountToHeal) : base(id, name, namePlural)
+        {
+            AmountToHeal = amountToHeal;
+        }
+    }
+}
